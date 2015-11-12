1dc8716628c6e90c0b8f6dd23724a492ea1ffebb
malike_st <st.malike@gmail.com>
2015-11-12 06:58:34 +0000

    Created security config for client application,Added application config to auth server and auth client

--- client/src/main/java/st/malike/auth/client/security/ResourceServerConfig.java
+++ client/src/main/java/st/malike/auth/client/security/ResourceServerConfig.java
@@ -6,10 +6,28 @@
 
 package st.malike.auth.client.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
 /**
  *
  * @author malike_st
  */
-public class ResourceServerConfig {
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+   
+    @Override 
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
 }
--- client/src/main/java/st/malike/auth/client/security/SecurityConfig.java
+++ client/src/main/java/st/malike/auth/client/security/SecurityConfig.java
@@ -6,10 +6,47 @@
 
 package st.malike.auth.client.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
 /**
  *
  * @author malike_st
  */
-public class SecurityConfig {
+@Configuration
+@EnableResourceServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${app.id}")
+    private String appId;
+    @Value("${app.client.secret}")
+    private String appSecret;
+    @Value("${auth.server.host}")
+    private String authServerHost;
+    @Value("${auth.server.port}")
+    private int authServerPort;
+
+    @Bean
+    public ResourceServerTokenServices tokenService() {
+        RemoteTokenServices tokenServices = new RemoteTokenServices();
+        tokenServices.setClientId(appId);
+        tokenServices.setClientSecret(appSecret);
+        tokenServices.setCheckTokenEndpointUrl("http://" + authServerHost + ":" + authServerPort + "/oauth/check_token");
+        return tokenServices;
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        OAuth2AuthenticationManager authenticationManager = new OAuth2AuthenticationManager();
+        authenticationManager.setTokenServices(tokenService());
+        return authenticationManager;
+    }
 }
