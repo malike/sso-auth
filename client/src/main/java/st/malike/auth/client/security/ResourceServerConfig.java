46d8d0f4b4263cbe7d0b43a40dcb56669affb8d4
malike_st <st.malike@gmail.com>
2015-11-12 07:10:07 +0000

    Created security config for client application,Added application config to auth server and auth client

--- client/src/main/java/st/malike/auth/client/security/ResourceServerConfig.java
+++ client/src/main/java/st/malike/auth/client/security/ResourceServerConfig.java
@@ -6,7 +6,6 @@
 
 package st.malike.auth.client.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
